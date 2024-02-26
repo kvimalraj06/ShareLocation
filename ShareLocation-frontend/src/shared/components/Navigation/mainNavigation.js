72c82877a84ebd39cf62abcfe209d368e5957fa4
de spook <kvimalraj0611@gmail.com>
2024-02-26 22:55:42 +0530

    Implemented Sidebar for mobile view

--- ShareLocation-frontend/src/shared/components/Navigation/mainNavigation.js
+++ ShareLocation-frontend/src/shared/components/Navigation/mainNavigation.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./mainHeader";
+import SideDrawer from "./sideDrawer";
 import NavLinks from "./navLinks";
+import Backdrop from "../UIElements/Backdrop";
+
 import "./mainNavigation.css";
 
 const MainNavigation = (props) => {
-  return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to={"/"}>Your Places</Link>
-      </h1>
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+  const handleDrawerOpen = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <>
+      {drawerIsOpen && <Backdrop onClick={() => handleDrawerClose()} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav
+            className="main-navigation__drawer-nav"
+            onClick={() => handleDrawerClose()}
+          >
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={() => handleDrawerOpen()}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to={"/"}>Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
